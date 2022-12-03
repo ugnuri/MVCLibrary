002750c59a4eedf99c21fe4b7bd708693dc1f3ee
Irungu <Irungu@INKHPLP>
2022-12-03 07:14:46 +0300

    Add project files.

---
+++ MVCLibrary/Migrations/20221202143207_initialcreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    CallNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.BookID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
---
+++ MVCLibrary/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCLibrary.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        [MaxLength (255)]
+        public string Title { get; set; }
+        [MaxLength(50)]
+        public string CallNumber { get; set; }
+        [MaxLength (50)]
+        public string Author { get; set; }
+    }
+}
